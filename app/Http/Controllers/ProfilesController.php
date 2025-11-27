6d2fb6f18b1044b94ac45f2b5066efbe07bcbd00
KunalSanghvii <kunalsanghavi08@gmail.com>
2025-11-26 21:37:13 -0500

    Edit profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -16,11 +17,41 @@ class ProfilesController extends Controller
         ]);
     }
 
-    public function edit(\App\User $user) #optimal way to do this
+    public function edit(User $user) #optimal way to do this
     {
-        $user = User::findOrFail($user);
-        return view('profiles.index',[
-            'user' => $user,
-        ]);
+        $this->authorize('update', $user->profile);
+        return view('profiles.edit', compact('user'));
+
+    }
+
+    public function update(User $user) #optimal way to do this
+    {
+       $data = request()->validate([
+        'title' => 'required',
+        'description' => 'required',
+        'url' => 'url',
+        'image' => '',
+       ]);
+
+
+
+       if (request('image')) {
+            $imagePath = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))
+                                 ->resize(1200, null, function ($constraint) {
+                                     $constraint->aspectRatio();
+                                     $constraint->upsize();
+                                 })
+                                 ->resizeCanvas(1000, 1000, 'center', false, 'ffffff');
+            $image->save();
+
+       }
+        auth()->$user->profile->update($array_merge(
+            $data,
+            ['image' => $imagePath]);
+        return redirect("/profile/{$user->id}");
+
+
     }
 }
